2df393e8671b510a59d2d5ffb26c22a828937cf6
Alexander Beck <alexander.beck@microsoft.com>
2022-12-01 12:05:39 +0100

    Solved Day1

--- AdventOfCode.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ AdventOfCode.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -1,17 +1,51 @@
 import Foundation
 
-
-//Get Content from input page
-var content: String = ""
-
-if let url = URL(string: "https://adventofcode.com/2022/day/1/input"){
+func GetInput() -> [String]{
     do{
-        content = try String(contentsOf: url)
-    } catch {
+        //Get Content from input file
+        let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+        let content: String = try String(contentsOf: fileURL!)
+        //Build array from input
+        return content.components(separatedBy: "\n")
+    } catch{
         print(error.localizedDescription)
     }
-    print(content)
+    fatalError()
+}
+    
+func CalculateCaloriesPerElf(input: [String]) -> [Int: Int]{
+    var input = GetInput()
+    //Sum up the calories carried by elfes
+    var elfCounter: Int = 1
+    var caloriesCounter: Int = 0
+    var CaloriesBacklog = [Int: Int]()
+    for calories in input{
+        if calories != "" {
+            caloriesCounter += Int(calories)!
+        }
+        else{
+            //Save results
+            CaloriesBacklog[elfCounter] = caloriesCounter
+            //Reset Calories counter
+            caloriesCounter = 0
+            //Calculate the secone elf
+            elfCounter += 1
+        }
+    }
+    return CaloriesBacklog
+}
+//Sort descending
+var elfBacklog = CalculateCaloriesPerElf(input: GetInput()).sorted{
+    //Sort descending
+    $0.value > $1.value
 }
 
+//Calculate top three
+var topThree = elfBacklog.prefix(3)
+var topThreetotal: Int = 0
+for elf in topThree{
+    topThreetotal += elf.value
+}
 
-
+print("Elf ",elfBacklog.first!.key, " is carrying the most (", elfBacklog.first!.value, ") calories")
+print("The first three elves carry: ", topThreetotal, "calories")
