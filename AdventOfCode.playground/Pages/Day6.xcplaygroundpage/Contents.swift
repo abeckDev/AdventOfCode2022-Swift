cf61750bc20695c937006de9dbd0879ab5d49435
Alexander Beck <alexander.beck@microsoft.com>
2022-12-06 09:47:44 +0100

    Added Day 6

---
+++ AdventOfCode.playground/Pages/Day6.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func GetInput() -> [Character]{
+    do{
+        //Get Content from input file
+        let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+        let content: String = try String(contentsOf: fileURL!)
+        //Build character array
+        return content.map({$0})
+    } catch{
+        print(error.localizedDescription)
+    }
+    fatalError()
+}
+
+//Get Input
+let input = GetInput()
+
+func GetMarkerLenght(lenght: Int, chars: [Character]) -> Int{
+    for i in 0 ... (chars.count - lenght - 1) {
+        //Check if the marker is in the current position (4 is minimu, so add it but remove the counter in the end (i+4-1)
+        let marker = chars[i ... i + lenght - 1]
+        //If the marker contains the exac and unique elements of four return solution
+        if Set(marker).count == lenght {
+            return (i + lenght)
+        }
+    }
+    fatalError("Not supposed to happen")
+}
+
+print("We need to process:",GetMarkerLenght(lenght: 4, chars: input), "Characters until the start package marker was detected!")
+
+
+print("We need to process:",GetMarkerLenght(lenght: 14, chars: input), "Characters until the start message marker was detected!")
